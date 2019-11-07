44a36827c8ca9365c3fa058cc9e8e41c5e1b0bc7
Erwin Heba <e.heba@getprintbox.com>
2019-11-07 12:37:19 +0100

    summary done

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,12 +1,20 @@
+import { observer } from 'mobx-react';
 import React from 'react';
 import { SummaryStyles } from '../styles/Summary';
+import { CurrencyUtil } from '../utils/CurrencyUtil';
+import { ExpensesUtil } from '../utils/ExpensesUtil';
 
-const Summary = () => {
+const Summary = observer(() => {
+	const summaryPLN = ExpensesUtil.getSummaryInPLN();
+	const summaryEUR = parseFloat(CurrencyUtil.PLNtoEUR(summaryPLN).toFixed(2));
 	return (
 		<SummaryStyles>
-			<h2 className='Summary__title'>Summary:</h2> <p className='Summary__count'>122.55 PLN (27.96 EUR)</p>
+			<h2 className='Summary__title'>Summary:</h2>{' '}
+			<p className='Summary__count'>
+				{summaryPLN} PLN ({summaryEUR} EUR)
+			</p>
 		</SummaryStyles>
 	);
-};
+});
 
 export default Summary;
